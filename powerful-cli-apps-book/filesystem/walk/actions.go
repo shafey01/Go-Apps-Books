d9410776259ff230276af8473d4ecf790fa22cf7
AhmedShafey01 <ahmed.shafey@squadio.com>
2024-08-08 10:32:28 +0300

    Walk tool

---
+++ powerful-cli-apps-book/filesystem/walk/actions.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func filterOut(path string, ext string, size int64, info os.FileInfo) bool {
+
+	if info.IsDir() || info.Size() < size {
+		return true
+	}
+
+	if ext != "" && filepath.Ext(path) != ext {
+		return true
+	}
+
+	return false
+
+}
+
+func listFile(path string, out io.Writer) error {
+	_, err := fmt.Fprintln(out, path)
+	return err
+}
+
+func delFile(path string, delLogger *log.Logger) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+	delLogger.Println(path)
+	return nil
+}
---
+++ powerful-cli-apps-book/filesystem/walk/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// • -root: The root of the directory tree to start the search.
+//    The default is the current directory.
+// • -list: List files found by the tool.
+//   When specified, no other actions will be executed.
+// • -ext: File extension to search.
+//    When specified, the tool will only match files
+//    with this extension.
+// • -size: Minimum file size in bytes.
+//    When specified, the tool will only match
+//   files whose size is larger than this value.
+//
+
+// config struct
+
+type config struct {
+	list bool
+	ext  string
+	size int64
+	del  bool
+	wLog io.Writer
+}
+
+func main() {
+
+	// flags
+	root := flag.String("root", ".", "Root path to start")
+	list := flag.Bool("list", false, "Just list files")
+	ext := flag.String("ext", "", "File extension to search")
+	size := flag.Int64("size", 0, "File size to search")
+	del := flag.Bool("del", false, "Delete file")
+	logFile := flag.String("log", "", "Log delted files")
+	flag.Parse()
+
+	// Open file for writing logs
+	var (
+		f   = os.Stdout
+		err error
+	)
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
+	// instant from config struct
+	c := config{
+		list: *list,
+		ext:  *ext,
+		size: *size,
+		del:  *del,
+		wLog: f,
+	}
+
+	// Run function
+
+	if err := Run(*root, os.Stdout, c); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func Run(root string, out io.Writer, cfg config) error {
+
+	delLogger := log.New(cfg.wLog, "DELETED File: ", log.LstdFlags)
+	return filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if filterOut(path, cfg.ext, cfg.size, info) {
+				return nil
+			}
+
+			if cfg.list {
+				return listFile(path, out)
+			}
+			if cfg.del {
+				return delFile(path, delLogger)
+			}
+			return listFile(path, out)
+		})
+}
